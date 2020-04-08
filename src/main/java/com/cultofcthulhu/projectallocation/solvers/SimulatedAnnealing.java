52ee68a5be0ae552c1f24e28df7e2ca929f639d3
rohan.shivakumar <rohan.shivakumar@ucdconnect.ie>
2020-04-08 15:36:56 +0100

    COMMIT

--- src/main/java/com/cultofcthulhu/projectallocation/solvers/SimulatedAnnealing.java
+++ src/main/java/com/cultofcthulhu/projectallocation/solvers/SimulatedAnnealing.java
@@ -1,28 +1,12 @@
 package com.cultofcthulhu.projectallocation.solvers;
 
-import com.cultofcthulhu.projectallocation.models.Project;
-import com.cultofcthulhu.projectallocation.models.Student;
-import com.cultofcthulhu.projectallocation.models.data.ProjectDAO;
 import com.cultofcthulhu.projectallocation.models.data.Solution;
-import com.cultofcthulhu.projectallocation.models.data.StudentDAO;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class SimulatedAnnealing {
-    private Solution currentBest;
-    private ProjectDAO projectDAO;
-    private StudentDAO studentDAO;
-    private Map<Student, Project> map;
+    public Solution currentBest;
 
-    public SimulatedAnnealing(Solution solution, ProjectDAO projectDAO, StudentDAO studentDAO) {
+    public SimulatedAnnealing(Solution solution) {
         currentBest = solution;
-        //Generate map of students to projects
-        Map<Student, Project> map = new HashMap<>();
-        for(Map.Entry<Integer, Integer> entry : solution.getSolution().entrySet()) {
-            map.put(studentDAO.getOne(entry.getKey()), projectDAO.getOne(entry.getValue()));
-        }
-        this.map = map;
     }
 
     public double assessSolution(Solution solution) {
