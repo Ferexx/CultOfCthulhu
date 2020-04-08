5de8ccb3436edd2d8f787d8109b0d61405c89ae4
Ferexx <jack.price99@hotmail.com>
2020-04-08 15:42:27 +0100

    Repush

--- src/main/java/com/cultofcthulhu/projectallocation/solvers/SimulatedAnnealing.java
+++ src/main/java/com/cultofcthulhu/projectallocation/solvers/SimulatedAnnealing.java
@@ -1,12 +1,23 @@
 package com.cultofcthulhu.projectallocation.solvers;
 
+import com.cultofcthulhu.projectallocation.models.Project;
+import com.cultofcthulhu.projectallocation.models.Student;
+import com.cultofcthulhu.projectallocation.models.data.ProjectDAO;
 import com.cultofcthulhu.projectallocation.models.data.Solution;
+import com.cultofcthulhu.projectallocation.models.data.StudentDAO;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SimulatedAnnealing {
     public Solution currentBest;
+    private ProjectDAO projectDAO;
+    private StudentDAO studentDAO;
 
-    public SimulatedAnnealing(Solution solution) {
+    public SimulatedAnnealing(Solution solution, ProjectDAO projectDAO, StudentDAO studentDAO) {
         currentBest = solution;
+        this.projectDAO = projectDAO;
+        this.studentDAO = studentDAO;
     }
 
     public double assessSolution(Solution solution) {
@@ -35,5 +46,13 @@ public class SimulatedAnnealing {
                 return true;
         }
         return false;
+    }
+
+    public Map<Student, Project> createDirectMap(Map<Integer, Integer> map) {
+        Map<Student, Project> newMap = new HashMap<>();
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            newMap.put(studentDAO.getOne(entry.getKey()), projectDAO.getOne(entry.getValue()));
+        }
+        return newMap;
     }
 }
