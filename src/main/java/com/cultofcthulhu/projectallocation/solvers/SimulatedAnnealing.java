4abcba1dc62458ca6a79f1fa6ec333750c04a980
Ferexx <jack.price99@hotmail.com>
2020-04-08 15:25:14 +0100

    First draft of hard constraints, with two hard constraints

--- src/main/java/com/cultofcthulhu/projectallocation/solvers/SimulatedAnnealing.java
+++ src/main/java/com/cultofcthulhu/projectallocation/solvers/SimulatedAnnealing.java
@@ -35,6 +35,16 @@ public class SimulatedAnnealing {
         Self-specified project can only be assigned to that student
         Maybe more
          */
+        Map<Student, Project> map = createDirectMap(solution.getSolution());
+        for(Map.Entry<Student, Project> entry : map.entrySet()) {
+            //First check if a project has been assigned to more than one person
+            for (Map.Entry<Student, Project> entry2 : map.entrySet())
+                if (entry.getKey().getAssignedProjectID() == entry2.getKey().getAssignedProjectID() && !entry.getKey().equals(entry2.getKey()))
+                    return true;
+            //Next check if a student has actually been assigned a project
+            if(entry.getKey().getAssignedProjectID() == -1)
+                return true;
+        }
         return false;
     }
 
