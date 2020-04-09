74d89ec858ab9f771082d844ee14726fe09fac4f
Ferexx <jack.price99@hotmail.com>
2020-04-09 21:13:36 +0100

    Add method for lecturer login page

--- src/main/java/com/cultofcthulhu/projectallocation/controllers/StudentController.java
+++ src/main/java/com/cultofcthulhu/projectallocation/controllers/StudentController.java
@@ -3,6 +3,7 @@ package com.cultofcthulhu.projectallocation.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -12,5 +13,11 @@ public class StudentController {
     public String preferencePage(@RequestParam long studentNum, Model model) {
         model.addAttribute("title", "Preferences");
         return "preferences";
+    }
+
+    @RequestMapping(value = "/login")
+    public String loginPage(Model model) {
+        model.addAttribute("title", "Log In");
+        return "login";
     }
 }
