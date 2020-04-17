28aa60ce46c1472febac13da14b8a9925159dc64
Ferexx <jack.price99@hotmail.com>
2020-04-17 03:36:46 +0100

    Added an option to allow users to upload their own students file. Needs to be tested thoroughly

--- src/main/java/com/cultofcthulhu/projectallocation/controllers/UploadController.java
+++ src/main/java/com/cultofcthulhu/projectallocation/controllers/UploadController.java
@@ -2,6 +2,8 @@ package com.cultofcthulhu.projectallocation.controllers;
 
 import com.cultofcthulhu.projectallocation.FileParser;
 import com.cultofcthulhu.projectallocation.exceptions.ParseException;
+import com.cultofcthulhu.projectallocation.models.Student;
+import com.cultofcthulhu.projectallocation.models.data.StudentDAO;
 import com.cultofcthulhu.projectallocation.storage.StorageFileNotFoundException;
 import com.cultofcthulhu.projectallocation.storage.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,8 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Controller
@@ -27,6 +31,9 @@ public class UploadController {
     public UploadController(StorageService storageService) {
         this.storageService = storageService;
     }
+
+    @Autowired
+    public StudentDAO studentDAO;
 
     @RequestMapping(value = "")
     @GetMapping("/")
@@ -49,7 +56,7 @@ public class UploadController {
                 "attachment; filename=\"" + file.getFilename() + "\"").body(file);
     }
 
-    @PostMapping(value = "/upload")
+    @PostMapping(value = "/uploadStaff")
     public String singleFileUpload(@RequestParam("file") MultipartFile file, Model model) {
         storageService.store(file);
         model.addAttribute("message", "You successfully uploaded '" + file.getOriginalFilename() + "'");
@@ -62,6 +69,7 @@ public class UploadController {
             return "error";
         }
     }
+
     @ExceptionHandler(StorageFileNotFoundException.class)
     public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
         return ResponseEntity.notFound().build();
@@ -70,5 +78,26 @@ public class UploadController {
     @GetMapping("/uploadStatus")
     public String uploadStatus() {
         return "uploadStatus";
+    }
+
+    @PostMapping("/uploadStudents")
+    public String uploadStudents(@RequestParam("file") MultipartFile file, Model model) {
+        storageService.store(file);
+        File convFile = new File(String.valueOf(storageService.load(file.getOriginalFilename())));
+        try {
+            List<Student> students =  parser.parseStudents(convFile);
+            for(Student student:students)
+                studentDAO.save(student);
+            model.addAttribute("message", "Students file uploaded.");
+            return "downloadProjects";
+        } catch (ParseException | IOException | NumberFormatException e) {
+            if(e.getClass() == ParseException.class)
+                model.addAttribute("error", e.getMessage());
+            else if(e.getClass() == IOException.class)
+                model.addAttribute("error", "Internal error, please restart the process.");
+            else if(e.getClass() == NumberFormatException.class)
+                model.addAttribute("error", "Please ensure your preferences are stored as integers corresponding to project IDs, and GPA is stored as an integer or double");
+            return "error";
+        }
     }
 }
