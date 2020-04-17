eed13b6585a4358015ca57621a4ca89a5461eccd
Ferexx <jack.price99@hotmail.com>
2020-04-17 14:56:09 +0100

    Testing for allowing users to upload their own student files

--- src/main/java/com/cultofcthulhu/projectallocation/FileParser.java
+++ src/main/java/com/cultofcthulhu/projectallocation/FileParser.java
@@ -3,6 +3,7 @@ package com.cultofcthulhu.projectallocation;
 import com.cultofcthulhu.projectallocation.exceptions.ParseException;
 import com.cultofcthulhu.projectallocation.models.Project;
 import com.cultofcthulhu.projectallocation.models.Student;
+import com.cultofcthulhu.projectallocation.system.systemVariables;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -13,6 +14,7 @@ public class FileParser {
     public List<String[]> lines = new ArrayList<>();
     private File toParse;
 
+    public FileParser() {}
     //Constructor does the actual parsing
     public FileParser(File toParse) throws ParseException {
         this.toParse = toParse;
@@ -46,20 +48,26 @@ public class FileParser {
         String split;
         List<Student> students = new ArrayList<>();
         if(file.getName().endsWith("csv")) split = ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)";
-        else if(file.getName().endsWith("tsv")) split = "\t";
-        else throw new ParseException("Please make sure your file is in .csv or .tsv format (values separated by commas or tabs, respectively), and saved as such.");
+        else if(file.getName().endsWith("tsv")) split = "\t(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)";
+        else throw new ParseException(
+                "Please make sure your file is in .csv or .tsv format (values separated by commas or tabs, respectively), and saved as such.");
         String line;
         BufferedReader br = new BufferedReader(new FileReader(file));
         int i = 1;
         while((line = br.readLine())!=null) {
             String[] values = line.split(split, -1);
-            if(values.length != 5) throw new ParseException("Your file has an incorrect number of fields on line " + i + ". (Found: " + values.length + ", Expected: 5)");
+            if(values.length != 5) throw new ParseException(
+                    "Your file has an incorrect number of fields on line " + i + ". (Found: " + values.length + ", Expected: 5)");
             values[3] = values[3].substring(1, values[3].length()-1);
             String[] preferences = values[3].split(split);
+            //TODO: Change 10 to numofstudents in sysvariables
+            if(preferences.length != 10) throw new ParseException(
+                    "The student on line " + i + " does not have the correct number of preferences. (Found: " + preferences.length + ", Expected: " + 10 + ")");
             Student student = new Student(values[0], values[1], values[2], Double.parseDouble(values[4]));
             for(String string : preferences)
                 student.addPreference(Integer.parseInt(string));
             students.add(student);
+            i++;
         }
         return students;
     }
--- src/test/java/com/cultofcthulhu/projectallocation/FileParserTest.java
+++ src/test/java/com/cultofcthulhu/projectallocation/FileParserTest.java
@@ -4,6 +4,7 @@ import com.cultofcthulhu.projectallocation.exceptions.ParseException;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -11,15 +12,33 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class FileParserTest {
 
     @Test
-    public void testParse() throws ParseException {
-        FileParser csvWorks = new FileParser(new File("test-files/ShouldWork.csv"));
-        FileParser tsvWorks = new FileParser(new File("test-files/ShouldWork.tsv"));
+    public void testParse() throws ParseException, IOException {
+        FileParser StaffCsvWorks = new FileParser(new File("test-files/Staff/ShouldWork.csv"));
+        FileParser StaffTsvWorks = new FileParser(new File("test-files/Staff/ShouldWork.tsv"));
 
         Exception exception = assertThrows(ParseException.class, () -> {
-            FileParser parser = new FileParser(new File("test-files/WontWork.csv"));
+            FileParser parser = new FileParser(new File("test-files/Staff/WontWork.csv"));
         });
         String expectedMessage = "Your file has an incorrect number of fields";
         String actualMessage = exception.getMessage();
         assertTrue(actualMessage.contains(expectedMessage));
+
+        FileParser StudentParsingWorks = new FileParser();
+        StudentParsingWorks.parseStudents(new File("test-files/Student/ShouldWork.csv"));
+        StudentParsingWorks.parseStudents(new File("test-files/Student/ShouldWork.tsv"));
+
+        Exception exception1 = assertThrows(ParseException.class, () -> {
+            StudentParsingWorks.parseStudents(new File("test-files/Student/IncorrectFields.csv"));
+        });
+        String actualMessage1 = exception1.getMessage();
+        assertTrue(actualMessage1.contains(expectedMessage));
+        Exception exception2 = assertThrows(ParseException.class, () -> {
+            StudentParsingWorks.parseStudents(new File("test-files/Student/IncorrectNumberOfPreferences.csv"));
+        });
+        String actualMessage2 = exception2.getMessage();
+        assertTrue(actualMessage2.contains("does not have the correct number of preferences"));
+        assertThrows(NumberFormatException.class, () -> {
+            StudentParsingWorks.parseStudents(new File("test-files/Student/IncorrectPreferenceType.csv"));
+        });
     }
 }
